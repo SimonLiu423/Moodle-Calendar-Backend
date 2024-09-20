b5926aeccca9f9a34900f32442e3773ff77cd726
SimonLiu <simonliu423@gmail.com>
2024-09-20 15:03:35 +0800

    Feat: config path argument for calendar_sync.sync

--- calendar_sync/sync/__main__.py
+++ calendar_sync/sync/__main__.py
@@ -1,6 +1,13 @@
 """
 Runs main.sync with default config.
 """
+import sys
+
 from . import config, main
 
-main.sync(config=config.load_config())
+config_path = None
+
+if len(sys.argv) > 1:
+    config_path = sys.argv[1]
+
+main.sync(config=config.load_config(config_path))
